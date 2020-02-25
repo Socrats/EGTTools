25e8309f43beba578b0ee98bf0710035cf729e7a
eliasfernandez <eliasfernandez.d@gmail.com>
2020-02-25 18:23:44 +0100

    added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='EGTtools',
+    version='0.0.1',
+    packages=['egttools', 'egttools.tests', 'egttools.analytical'],
+    url='',
+    license='MIT License',
+    author='Elias Fernanez',
+    author_email='elias.fernandez.domingos@vub.be',
+    description='Computational tools for studying Game Theoretical problems '
+                'using the Evolutionary Game Theory Framework.'
+)
