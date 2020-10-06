0d482163aec1496ac773eb285fb586a21e859675
eliasfernandez <eliasfernandez.d@gmail.com>
2020-10-06 15:41:37 +0200

    updated setup info

--- setup.py
+++ setup.py
@@ -18,13 +18,43 @@
 from setuptools import setup
 
 setup(
-    name='EGTtools',
-    version='0.0.1',
-    packages=['egttools', 'egttools.tests', 'egttools.analytical', 'egttools.plotting'],
-    url='',
-    license='MIT License',
+    name='egttools',
+    version='0.1.0',
+    description='Computational tools for studying Game Theoretical problems '
+                'using the Evolutionary Game Theory Framework.',
+    url="https://github.com/Socrats/EGTTools",
+    project_urls={
+        "Bug Tracker": "",
+        "Documentation": "",
+        "Source Code": "https://github.com/Socrats/EGTTools",
+    },
     author='Elias Fernandez',
     author_email='elias.fernandez.domingos@vub.be',
-    description='Computational tools for studying Game Theoretical problems '
-                'using the Evolutionary Game Theory Framework.'
+    license='GPLv3',
+    classifiers=[
+        'Development Status :: 3 - Testing/Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX :: Linux',
+        'Operating System :: Unix',
+        'Programming Language :: C++',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    keywords=["evolutionary game theory", "social dynamics", "replicator dynamics"],
+    python_requires='>=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*',
+    install_requires=[
+        'numpy>=1.7.0',
+    ],
+    zip_safe=False,
 )
