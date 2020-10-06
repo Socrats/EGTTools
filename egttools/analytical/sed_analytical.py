a90353e272ec05522c85836a6a0200654a4a8782
eliasfernandez <eliasfernandez.d@gmail.com>
2020-10-06 15:29:22 +0200

    Adding GPL preamble to all files

--- egttools/analytical/sed_analytical.py
+++ egttools/analytical/sed_analytical.py
@@ -1,6 +1,21 @@
-"""
-Copyright (c) 2019 Elias Fernandez
+# Copyright (c) 2019-2020  Elias Fernandez
+#
+# This file is part of EGTtools.
+#
+# EGTtools is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# EGTtools is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with EGTtools.  If not, see <http://www.gnu.org/licenses/>
 
+"""
 This python module contains the necessary functions
 to calculate analytically the evolutionary dynamics in Infinite and Finite
 populations on 2-player games.
