ad1bee3cbae50268792dd4df8de4f749bd5bd672
eliasfernandez <eliasfernandez.d@gmail.com>
2021-03-09 13:22:18 +0100

    added full fitness and gradient calculation - for a full state representation on problem of more than 2 strategies

--- egttools/__init__.py
+++ egttools/__init__.py
@@ -5,4 +5,6 @@ import egttools.analytical as analytical
 try:
     import egttools.numerical as numerical
 except ImportError:
-    print("Numerical module cannot be imported. It might not have been compiled correctly.")
+    raise ImportError("Numerical module cannot be imported. It might not have been compiled correctly.")
+else:
+    from egttools.numerical import (sample_simplex, calculate_nb_states, calculate_state, )
--- egttools/analytical/sed_analytical.py
+++ egttools/analytical/sed_analytical.py
@@ -22,11 +22,16 @@ populations on 2-player games.
 """
 
 import numpy as np
-from scipy.sparse import lil_matrix
-from scipy.stats import hypergeom
+from scipy.sparse import csr_matrix
+from scipy.stats import hypergeom, multivariate_hypergeom
+
+try:
+    from egttools.numerical import sample_simplex, calculate_nb_states
+except ImportError:
+    raise
 
 
-def replicator_equation(x, payoffs):
+def replicator_equation(x: np.ndarray, payoffs: np.ndarray) -> np.ndarray:
     """
     Produces the discrete time derivative of the replicator dynamics
 
@@ -50,24 +55,30 @@ class StochDynamics:
                 number of strategies in the population
     pop_size : int
             population size
-    payoffs : ndarray
+    payoffs : array_like
             payoff matrix indicating the payoff of each strategy (rows) against each other (columns)
     mu : float
         mutation probability
     """
 
-    def __init__(self, nb_strategies, payoffs, pop_size, group_size=2, mu=0):
+    def __init__(self, nb_strategies: int, payoffs: np.ndarray, pop_size: int, group_size=2, mu=0):
         self.nb_strategies = nb_strategies
         self.payoffs = payoffs
         self.Z = pop_size
         self.N = group_size
         self.mu = mu
+        self.nb_states_population = calculate_nb_states(pop_size, nb_strategies)
+        self.nb_group_combinations = calculate_nb_states(group_size, nb_strategies)
+        if group_size > 2:  # pairwise game
+            self.full_fitness = self.full_fitness_difference_group
+        else:  # group game
+            self.full_fitness = self.full_fitness_difference_pairwise
         if group_size > 2:  # pairwise game
             self.fitness = self.fitness_group
         else:  # group game
             self.fitness = self.fitness_pair
 
-    def fitness_pair(self, x, i, j, *args):
+    def fitness_pair(self, x: int, i: int, j: int, *args: {None, list}) -> float:
         """
         Calculates the fitness of strategy i versus strategy j, in
         a population of x i-strategists and (Z-x) j strategists, considering
@@ -81,10 +92,11 @@ class StochDynamics:
             index of strategy i
         j : int
             index of strategy j
+        args : {None, List}, optional
 
         Returns
         -------
-            int
+            float
             the fitness difference among the strategies
         """
         fitness_i = ((x - 1) * self.payoffs[i, i] +
@@ -93,7 +105,41 @@ class StochDynamics:
                      x * self.payoffs[j, i]) / (self.Z - 1)
         return fitness_i - fitness_j
 
-    def fitness_group(self, x, i, j, *args):
+    def full_fitness_difference_pairwise(self, i: int, j: int, population_state: np.ndarray) -> float:
+        """
+        Calculates the fitness of strategy i in a population with state :param population_state,
+        assuming pairwise interactions (2-player game).
+
+        Parameters
+        ----------
+        i : int
+            index of the strategy that will reproduce
+        j : int
+            index of the strategy that will die
+        population_state : array_like
+                           vector containing the counts of each strategy in the population
+        args : {None, List}, optional
+               extra arguments for the payoff matrix
+
+        Returns
+        -------
+        float
+        The fitness difference between the two strategies for the given population state
+        """
+        fitness_i = (population_state[i] - 1) * self.payoffs[i, i]
+        for strategy in range(self.nb_strategies):
+            if strategy == i:
+                continue
+            fitness_i += population_state[strategy] * self.payoffs[i, strategy]
+        fitness_j = (population_state[j] - 1) * self.payoffs[j, j]
+        for strategy in range(self.nb_strategies):
+            if strategy == j:
+                continue
+            fitness_j += population_state[strategy] * self.payoffs[j, strategy]
+
+        return (fitness_i - fitness_j) / (self.Z - 1)
+
+    def fitness_group(self, x: int, i: int, j: int, *args: {None, list}) -> float:
         """
         In a population of x i-strategists and (Z-x) j strategists, where players
         interact in group of 'group_size' participants this function
@@ -107,23 +153,64 @@ class StochDynamics:
             index of strategy i
         j : int
             index of strategy j
-        args : List
+        args : {None, List}, optional
             Other Parameters. This can be used to pass extra parameters to functions
             stored in the payoff matrix
 
         Returns
         -------
-            int
+            float
             Returns the difference in fitness between strategy i and j
         """
         k_array = np.arange(0, self.N, dtype=np.int32)
-        i_pmf = hypergeom(self.Z-1, x-1, self.N-1).pmf(k_array)
-        j_pmf = hypergeom(self.Z-1, x, self.N-1).pmf(k_array)
+        i_pmf = hypergeom(self.Z - 1, x - 1, self.N - 1).pmf(k_array)
+        j_pmf = hypergeom(self.Z - 1, x, self.N - 1).pmf(k_array)
 
         fitness_i, fitness_j = 0, 0
         for k in k_array:
-            fitness_i += self.payoffs[i, j](k + 1, self.N, *args)*i_pmf[k]
-            fitness_j += self.payoffs[j, i](self.N - k, self.N, *args)*j_pmf[k]
+            fitness_i += self.payoffs[i, j](k + 1, self.N, *args) * i_pmf[k]
+            fitness_j += self.payoffs[j, i](self.N - k, self.N, *args) * j_pmf[k]
+
+        return fitness_i - fitness_j
+
+    def full_fitness_difference_group(self, i: int, j: int, population_state: np.ndarray) -> float:
+        """
+        Calculate the fitness difference between strategies :param i and :param j
+        assuming that player interact in groups of size N > 2 (n-player games).
+
+        Parameters
+        ----------
+        i : int
+            index of the strategy that will reproduce
+        j : int
+            index of the strategy that will die
+        population_state : array_like
+                           vector containing the counts of each strategy in the population
+        args : {None, List}, optional
+
+        Returns
+        -------
+        float
+        The fitness difference between strategies i and j
+        """
+        population_state[i] -= 1
+        rv_i = multivariate_hypergeom(population_state, self.N - 1)
+        population_state[i] += 1
+        population_state[j] -= 1
+        rv_j = multivariate_hypergeom(population_state, self.N - 1)
+        population_state[j] += 1
+
+        fitness_i, fitness_j = 0., 0.
+        for state_index in range(self.nb_group_combinations):
+            group = sample_simplex(i, self.Z - 1, self.nb_strategies)
+            if group[i] > 0:
+                group[i] -= 1
+                fitness_i += self.payoffs[i, state_index] * rv_i.pmf(x=group)
+                group[i] += 1
+            if group[j] > 0:
+                group[i] -= 1
+                fitness_j += self.payoffs[j, state_index] * rv_j.pmf(x=group)
+                group[i] += 1
 
         return fitness_i - fitness_j
 
@@ -175,6 +262,41 @@ class StochDynamics:
         p_less = ((1 - self.mu) * p_less) + (self.mu * (k / self.Z))
         return p_plus, p_less
 
+    def full_prob_increase_decrease_with_mutation(self, population_state, beta, *args):
+        """
+
+        Parameters
+        ----------
+        population_state : array_like
+                           structure of unsigned integers containing the
+                           counts of each strategy in the population
+        beta : float
+               intensity of selection
+        args : {None, List}, optional
+               additional parameters necessary to compute the fitness
+
+        Returns
+        -------
+        Returns an ndarray matrix with the probabilities of increasing/decreasing one individual adopting
+        a given strategy in the current population state.
+        All possible new state transition probabilities are returned.
+
+        e.g., given the population state (34, 33, 33) which represents a population of size 100 with 3
+        strategies - obviously the population state may be represented with a 2D tuple (34, 33) - then
+        there are 6 possible changes to the population: (35, 32, 33), (35, 33, 32), (33, 34, 33),
+        (33, 33, 34), (34, 34, 32), (34, 32, 34).
+        """
+        transitions = np.zeros(shape=(2 * self.nb_strategies, 2 * self.nb_strategies))
+        for i in range(self.nb_strategies):
+            # If count of strategy < Z we calculate the probability of increasing
+            if population_state[i] < self.Z:
+                # calculate p_increase
+                pass
+            if population_state[i] > 0:
+                # calculate p_decrease
+                pass
+        return transitions
+
     def gradient_selection(self, k, invader, resident, beta, *args):
         """
         Calculates the gradient of selection given an invader and a resident strategy.
@@ -186,8 +308,31 @@ class StochDynamics:
         :return: gradient of selection
         :rtype: float
         """
-        return ((self.Z - k) / float(self.Z)) * (k / float(self.Z)) * np.tanh(
+        return ((self.Z - k) / float(self.Z)) * (k / float(self.Z - 1)) * np.tanh(
             (beta / 2) * self.fitness(k, invader, resident, *args))
+
+    def full_gradient_selection(self, population_state: np.ndarray, beta: float) -> np.ndarray:
+        """
+        Calculates the gradient of selection for an invading strategy, given a population satte.
+
+        Parameters
+        ----------
+        population_state : array_like structure of unsigned integers containing the
+                           counts of each strategy in the population
+        beta : intensity of selection
+        args : {None, List}, optional
+               additional parameters necessary to compute the fitness
+
+        Returns an ndarray matrix indicating the likelihood of change in the population given an starting point.
+        -------
+
+        """
+        probability_selecting_strategy_first = population_state / self.Z
+        probability_selecting_strategy_second = population_state / (self.Z - 1)
+        probabilities = np.outer(probability_selecting_strategy_first, probability_selecting_strategy_second)
+        fitness = np.asarray([[self.full_fitness(i, j, population_state) for i in
+                               range(len(population_state))] for j in range(len(population_state))])
+        return probabilities * np.tanh(beta / 2) * fitness
 
     def fixation_probability(self, invader, resident, beta, *args):
         """
@@ -196,7 +341,7 @@ class StochDynamics:
 
         The fixation probability is derived analytically:
 
-        $\phi = $
+        \( \phi = $ \)
 
         :param invader: index of the invading strategy
         :param resident: index of the resident strategy
@@ -220,7 +365,7 @@ class StochDynamics:
         :param beta: intensity of selection
         :return full transition matrix between the two strategies
         """
-        transitions = lil_matrix((self.Z + 1, self.Z + 1), dtype=np.float64)
+        transitions = csr_matrix((self.Z + 1, self.Z + 1), dtype=np.float64)
         # Case of 0:
         transitions[0, 1] = self.mu
         transitions[0, 0] = 1. - self.mu
