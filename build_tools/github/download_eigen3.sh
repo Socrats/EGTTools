3ac2cb6979b66b119ab50542f8078f86a607b9bf
eliasfernandez <Socrats@users.noreply.github.com>
2021-05-02 11:19:53 +0200

    install cmake on linux

--- build_tools/github/download_eigen3.sh
+++ build_tools/github/download_eigen3.sh
@@ -7,6 +7,8 @@ if [[ "$OSTYPE" == "linux-gnu" ]]; then
     # The Linux test environment is run in a Docker container and
     # it is not possible to copy the test configuration file (yet)
 
+    pip install cmake
+    ln -s "$(which cmake)" /usr/local/bin/cmake
 
     # First we download the correct eigen3 version and build it
     curl -O  https://gitlab.com/libeigen/eigen/-/archive/3.3.9/eigen-3.3.9.tar.gz
