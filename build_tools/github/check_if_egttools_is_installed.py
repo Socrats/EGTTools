468cfc807d3c8b45dd46222a1761c99766b84f9c
eliasfernandez <Socrats@users.noreply.github.com>
2021-05-02 15:11:18 +0200

    updates to fix pytest cibuildwheel error

---
+++ build_tools/github/check_if_egttools_is_installed.py
@@ -0,0 +1,6 @@
+try:
+    import egttools
+except ImportError as e:
+    print('egttools is not installed')
+else:
+    print('found egttools')
--- build_tools/github/test_wheels.sh
+++ build_tools/github/test_wheels.sh
@@ -3,4 +3,8 @@
 set -e
 set -x
 
-pytest --rootdir="$1"
+# Check if python is installed
+python "$1/build_tools/github/check_if_egttools_is_installed.py"
+
+#pip install pytest-github-actions-annotate-failures
+pytest
