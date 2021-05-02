e1e0e57c376092fd8e4a1810290c201cb34eb17d
eliasfernandez <Socrats@users.noreply.github.com>
2021-05-02 11:16:58 +0200

    build eigen3

--- build_tools/github/download_eigen3.sh
+++ build_tools/github/download_eigen3.sh
@@ -8,15 +8,21 @@ if [[ "$OSTYPE" == "linux-gnu" ]]; then
     # it is not possible to copy the test configuration file (yet)
 
 
-    # First we download the correct eigen3 version
+    # First we download the correct eigen3 version and build it
     curl -O  https://gitlab.com/libeigen/eigen/-/archive/3.3.9/eigen-3.3.9.tar.gz
     tar xf eigen-3.3.9.tar.gz
     mv eigen-3.3.9 eigen3
+    cd eigen3
+    mkdir "build"
+    cd build
+    cmake ..
+    make install
 
-    EIGEN_PATH="$(cd "$(dirname "$1")"; pwd -P)/$(basename "$1")eigen3"
 
-    export CFLAGS="$CFLAGS -I$EIGEN_PATH/include"
-    export CXXFLAGS="$CXXFLAGS -I$EIGEN_PATH/include"
-    export Eigen3_DIR="$EIGEN_PATH/cmake"
-    export CIBW_ENVIRONMENT="$CIBW_ENVIRONMENT Eigen3_DIR=$Eigen3_DIR"
+#    EIGEN_PATH="$(cd "$(dirname "$1")"; pwd -P)/$(basename "$1")eigen3"
+#
+#    export CFLAGS="$CFLAGS -I$EIGEN_PATH/include"
+#    export CXXFLAGS="$CXXFLAGS -I$EIGEN_PATH/include"
+#    export Eigen3_DIR="$EIGEN_PATH/cmake/"
+#    echo "Eigen3_DIR=$Eigen3_DIR" >> CIBW_ENVIRONMENT
 fi
