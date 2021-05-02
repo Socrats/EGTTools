2d91f51990db7c27b7696ccf321405cb14fab250
eliasfernandez <Socrats@users.noreply.github.com>
2021-05-02 09:55:48 +0200

    fixed test_wheels.sh

--- build_tools/github/test_wheels.sh
+++ build_tools/github/test_wheels.sh
@@ -3,10 +3,4 @@
 set -e
 set -x
 
-if [[ "$OSTYPE" != "linux-gnu" ]]; then
-    # The Linux test environment is run in a Docker container and
-    # it is not possible to copy the test configuration file (yet)
-    cp "$CONFTEST_PATH" "$CONFTEST_NAME"
-fi
-
-pytest
+pytest --rootdir="$1"
