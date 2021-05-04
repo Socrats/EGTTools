136f7c6f9ff5a8b2ff333508cfc08b2386afebc0
eliasfernandez <Socrats@users.noreply.github.com>
2021-05-04 21:13:26 +0200

    trying to make windows built work

--- build_tools/github/download_eigen3.sh
+++ build_tools/github/download_eigen3.sh
@@ -31,7 +31,8 @@ elif [[ "$RUNNER_OS" == "Windows" ]]; then
   curl -O https://gitlab.com/libeigen/eigen/-/archive/3.3.9/eigen-3.3.9.tar.gz
   tar xf eigen-3.3.9.tar.gz
   mv eigen-3.3.9 eigen3
-  export EIGEN3_INCLUDE_DIR="$(pwd)\eigen3"
+  EIGEN3_INCLUDE_DIR="$(pwd)/eigen3"
+  export EIGEN3_INCLUDE_DIR
   echo "EIGEN3_INCLUDE_DIR=$EIGEN3_INCLUDE_DIR"
   echo "EIGEN3_INCLUDE_DIR=$EIGEN3_INCLUDE_DIR" >> "$GITHUB_ENV"
   export CIBW_ENVIRONMENT="$CIBW_ENVIRONMENT EIGEN3_INCLUDE_DIR=$EIGEN3_INCLUDE_DIR"
