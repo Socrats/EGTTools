bb252815b1edb2914a9837c6c157bdff3db135a9
eliasfernandez <Socrats@users.noreply.github.com>
2021-05-04 11:38:41 +0200

    back to MACOSX_DEPLOYMENT_TARGET=10.3

--- build_tools/github/build_wheels.sh
+++ build_tools/github/build_wheels.sh
@@ -18,7 +18,8 @@ if [[ "$RUNNER_OS" == "macOS" ]]; then
 #      wget https://homebrew.bintray.com/bottles/libomp-11.0.0.high_sierra.bottle.tar.gz
 #      brew install libomp-11.0.0.high_sierra.bottle.tar.gz
 #    fi
-    brew install libomp
+    wget https://homebrew.bintray.com/bottles/libomp-11.0.0.high_sierra.bottle.tar.gz
+    brew install libomp-11.0.0.high_sierra.bottle.tar.gz
     export CC=/usr/bin/clang
     export CXX=/usr/bin/clang++
     export CPPFLAGS="$CPPFLAGS -Xpreprocessor -fopenmp -macosx-version-min $MACOSX_DEPLOYMENT_TARGET"
