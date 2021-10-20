df791c541e1b3a09647ee16b6af22c031c503c2c
eliasfernandez <Socrats@users.noreply.github.com>
2021-10-20 19:53:28 +0200

    added moving average crd strategies

--- egttools/behaviors/CRD/__init__.py
+++ egttools/behaviors/CRD/__init__.py
@@ -3,3 +3,4 @@ API reference documentation for `behaviors.CRD` submodule.
 """
 
 from egttools.numerical.behaviors.CRD import AbstractCRDStrategy, CRDMemoryOnePlayer
+from egttools.behaviors.CRD.moving_average import MovingAverageCRDStrategy
