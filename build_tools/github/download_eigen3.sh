0508356c3824a43ad9bf644bb0e4eaa37e40f42d
eliasfernandez <Socrats@users.noreply.github.com>
2021-10-27 12:55:29 +0200

    another try

--- build_tools/github/download_eigen3.sh
+++ build_tools/github/download_eigen3.sh
@@ -37,4 +37,34 @@ elif [[ "$RUNNER_OS" == "Windows" ]]; then
   export CIBW_ENVIRONMENT_WINDOWS="$CIBW_ENVIRONMENT EIGEN3_INCLUDE_DIR=$EIGEN3_INCLUDE_DIR"
   export CMAKE_MODULE_PATH="$CMAKE_MODULE_PATH $EIGEN3_INCLUDE_DIR$"
   export CIBW_ENVIRONMENT_WINDOWS="$CIBW_ENVIRONMENT_WINDOWS CMAKE_MODULE_PATH=$CMAKE_MODULE_PATH '--config Release'"
+elif [[ "$RUNNER_OS" == "macOS" ]]; then
+  # Make sure to use a libomp version binary compatible with the oldest
+  # supported version of the macos SDK as libomp will be vendored into the
+  # scikit-learn wheels for macos. The list of bottles can be found at:
+  # https://formulae.brew.sh/api/formula/libomp.json. Currently, the oldest
+  # supported macos version is: High Sierra / 10.13. When upgrading this, be
+  # sure to update the MACOSX_DEPLOYMENT_TARGET environment variable in
+  # wheels.yml accordingly.
+  if [[ "$BUILD_ARCH" == "macosx_x86_64"  ]]; then
+    wget https://homebrew.bintray.com/bottles/libomp-11.0.0.high_sierra.bottle.tar.gz
+    brew install libomp-11.0.0.high_sierra.bottle.tar.gz
+    export MACOSX_DEPLOYMENT_TARGET=10.13
+    export CIBW_ENVIRONMENT="$CIBW_ENVIRONMENT MACOSX_DEPLOYMENT_TARGET=10.13"
+  else
+    export MACOSX_DEPLOYMENT_TARGET=10.15
+    export CIBW_ENVIRONMENT="$CIBW_ENVIRONMENT MACOSX_DEPLOYMENT_TARGET=10.15"
+    brew install libomp
+  fi
+  export CC=/usr/bin/clang
+  export CXX=/usr/bin/clang++
+  export CPPFLAGS="$CPPFLAGS -Xpreprocessor -fopenmp"
+  export CFLAGS="$CFLAGS -I/usr/local/opt/libomp/include"
+  export CXXFLAGS="$CXXFLAGS -I/usr/local/opt/libomp/include"
+  export LDFLAGS="$LDFLAGS -Wl,-rpath,/usr/local/opt/libomp/lib -L/usr/local/opt/libomp/lib -lomp"
+
+  brew install eigen
+  brew install openblas
+
+  export LDFLAGS="$LDFLAGS -L/usr/local/opt/openblas/lib"
+  export CPPFLAGS="$CPPFLAGS -I/usr/local/opt/openblas/include"
 fi
