20a28890f50a3ca565a138fc6d1fcd2e338772f9
eliasfernandez <Socrats@users.noreply.github.com>
2021-11-03 11:01:14 +0100

    Fixed import errors and added new deployment targets

--- build_tools/github/download_eigen3.sh
+++ build_tools/github/download_eigen3.sh
@@ -3,39 +3,19 @@
 set -e
 set -x
 
-if [[ "$OSTYPE" == "linux-gnu" ]]; then
-  # The Linux test environment is run in a Docker container and
-  # it is not possible to copy the test configuration file (yet)
+# The Linux test environment is run in a Docker container and
+# it is not possible to copy the test configuration file (yet)
 
-  python -m pip install --upgrade --user pip cmake
+python -m pip install --upgrade --user pip cmake
 #  ln -s "$(which cmake)" /usr/local/bin/cmake
 
-  # First we download the correct eigen3 version and build it
-  curl -O https://gitlab.com/libeigen/eigen/-/archive/3.3.9/eigen-3.3.9.tar.gz
-  tar xf eigen-3.3.9.tar.gz
-  mv eigen-3.3.9 eigen3
-  cd eigen3
-  mkdir "build"
-  cd build
-  cmake ..
-  make install
-  yum install -y libjpeg-turbo-devel
-#  yum install -y eigen3-devel
-#  echo "Eigen3_DIR='/usr/include/eigen3'" >> $GITHUB_ENV
-elif [[ "$RUNNER_OS" == "Windows" ]]; then
-  #  curl.exe --output eigen-3.3.9.tar.gz --url https://gitlab.com/libeigen/eigen/-/archive/3.3.9/eigen-3.3.9.tar.gz
-  #  tar -zxvf eigen-3.3.9.tar.gz
-  #  ren eigen-3.3.9 eigen3
-  #  export EIGEN_INCLUDE_DIR="eigen3"
-  # First we download the correct eigen3 version and build it
-  curl -O https://gitlab.com/libeigen/eigen/-/archive/3.3.9/eigen-3.3.9.tar.gz
-  tar xf eigen-3.3.9.tar.gz
-  mv eigen-3.3.9 eigen3
-  EIGEN3_INCLUDE_DIR="$(pwd)/eigen3"
-  export EIGEN3_INCLUDE_DIR
-  echo "EIGEN3_INCLUDE_DIR=$EIGEN3_INCLUDE_DIR"
-  echo "EIGEN3_INCLUDE_DIR=$EIGEN3_INCLUDE_DIR" >> "$GITHUB_ENV"
-  export CIBW_ENVIRONMENT_WINDOWS="$CIBW_ENVIRONMENT EIGEN3_INCLUDE_DIR=$EIGEN3_INCLUDE_DIR"
-  export CMAKE_MODULE_PATH="$CMAKE_MODULE_PATH $EIGEN3_INCLUDE_DIR$"
-  export CIBW_ENVIRONMENT_WINDOWS="$CIBW_ENVIRONMENT_WINDOWS CMAKE_MODULE_PATH=$CMAKE_MODULE_PATH '--config Release'"
-fi
+# First we download the correct eigen3 version and build it
+curl -O https://gitlab.com/libeigen/eigen/-/archive/3.3.9/eigen-3.3.9.tar.gz
+tar xf eigen-3.3.9.tar.gz
+mv eigen-3.3.9 eigen3
+cd eigen3
+mkdir "build"
+cd build
+cmake ..
+make install
+yum install -y libjpeg-turbo-devel
---
+++ build_tools/github/download_eigen3_windows.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+set -x
+
+curl.exe --output eigen-3.3.9.tar.gz --url https://gitlab.com/libeigen/eigen/-/archive/3.3.9/eigen-3.3.9.tar.gz
+tar -zxvf eigen-3.3.9.tar.gz
+ren eigen-3.3.9 Eigen3
+move eigen3 "C:/Program Files (x86)/Eigen3"
+export Eigen3_DIR='C:/Program Files (x86)/Eigen3'
+echo "Eigen3_DIR='C:/Program Files (x86)/Eigen3'" >> $GITHUB_ENV
+export EGTTOOLS_EXTRA_CMAKE_ARGS='-DSKIP_OPENMP:BOOL=TRUE'
+echo "EGTTOOLS_EXTRA_CMAKE_ARGS='-DSKIP_OPENMP:BOOL=TRUE'" >> $GITHUB_ENV
