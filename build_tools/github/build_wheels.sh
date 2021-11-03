d02a999b2db149fddc7b0b6a651e6e70f31eda1d
eliasfernandez <Socrats@users.noreply.github.com>
2021-11-03 17:20:40 +0100

    try to build arm64

--- build_tools/github/build_wheels.sh
+++ build_tools/github/build_wheels.sh
@@ -17,10 +17,10 @@ if [[ "$RUNNER_OS" == "macOS" ]]; then
     wget https://homebrew.bintray.com/bottles/libomp-11.0.0.high_sierra.bottle.tar.gz
     brew install libomp-11.0.0.high_sierra.bottle.tar.gz
     export MACOSX_DEPLOYMENT_TARGET=10.9
-    export CIBW_ENVIRONMENT="$CIBW_ENVIRONMENT MACOSX_DEPLOYMENT_TARGET=10.13"
+    export CIBW_ENVIRONMENT="$CIBW_ENVIRONMENT MACOSX_DEPLOYMENT_TARGET=10.9"
   else
     export MACOSX_DEPLOYMENT_TARGET=10.9
-    export CIBW_ENVIRONMENT="$CIBW_ENVIRONMENT MACOSX_DEPLOYMENT_TARGET=10.15"
+    export CIBW_ENVIRONMENT="$CIBW_ENVIRONMENT MACOSX_DEPLOYMENT_TARGET=10.9"
     brew install libomp
   fi
   export CC=/usr/bin/clang
