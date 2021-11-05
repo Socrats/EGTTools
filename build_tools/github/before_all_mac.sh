3842f4e711bad1720cd8ec677aa992ff4a9af37d
eliasfernandez <Socrats@users.noreply.github.com>
2021-11-05 14:20:44 +0100

    moving to cibuildwheel@v2.2.2

---
+++ build_tools/github/before_all_mac.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+set -x
+
+brew update
+brew install eigen
+echo "EIGEN=$(brew --prefix eigen)" >> $GITHUB_ENV
+brew install gfortran
+echo "GFORTRAN=$(brew --prefix gfortran)" >> $GITHUB_ENV
+brew install openblas
+echo "OPENBLAS=$(brew --prefix openblas)" >> $GITHUB_ENV
+echo "EGTTOOLS_EXTRA_CMAKE_ARGS='-DSKIP_OPENMP:BOOL=TRUE'" >> $GITHUB_ENV
+brew install libomp
+echo "LIBOMP=$(brew --prefix libomp)" >> $GITHUB_ENV
