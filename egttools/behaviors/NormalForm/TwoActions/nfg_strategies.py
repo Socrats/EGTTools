c8d3d0cd74f08d9df6156e7cc77b0c1d27331df3
eliasfernandez <Socrats@users.noreply.github.com>
2021-11-12 18:19:11 +0100

    added new detective strategy

--- egttools/behaviors/NormalForm/TwoActions/nfg_strategies.py
+++ egttools/behaviors/NormalForm/TwoActions/nfg_strategies.py
@@ -1,9 +1,26 @@
+# Copyright (c) 2019-2021  Elias Fernandez
+#
+# This file is part of EGTtools.
+#
+# EGTtools is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# EGTtools is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with EGTtools.  If not, see <http://www.gnu.org/licenses/>
+
 from .. import AbstractNFGStrategy
 from egttools import Random
 
 import numpy as np
 
-__all__ = ['EpsilonTFT', 'EpsilonGRIM']
+__all__ = ['EpsilonTFT', 'EpsilonGRIM', 'Detective']
 
 
 class EpsilonTFT(AbstractNFGStrategy):
@@ -91,6 +108,42 @@ class EpsilonGRIM(AbstractNFGStrategy):
 
     def type(self):
         return "NFGStrategies::EpsilonGRIM"
+
+    def is_stochastic(self):
+        return self.is_stochastic_strategy
+
+    def __str__(self):
+        return self.type()
+
+
+class Detective(AbstractNFGStrategy):
+    def __init__(self):
+        """
+        A Detective player who tries to analyze the opponent.
+
+        This player will always play the same initial sequence of
+        Cooperate, Defect, Cooperate, Cooperate. If the opponent defects
+        during this initial sequence, then Defective will play TFT from the 5th
+        round on. Otherwise, Detective will play always Defect.
+        """
+        super().__init__()
+        self.initial_sequence_ = [1, 0, 1, 1]
+        self.cheated_ = False
+        self.is_stochastic_strategy = False
+
+    def get_action(self, time_step: int, action_prev: int):
+        if time_step < 4:
+            if time_step > 0 and action_prev == 0:
+                self.cheated_ = True
+            return self.initial_sequence_[time_step]
+        else:
+            if self.cheated_:
+                return action_prev
+            else:
+                return 0
+
+    def type(self):
+        return "NFGStrategies::Detective"
 
     def is_stochastic(self):
         return self.is_stochastic_strategy
