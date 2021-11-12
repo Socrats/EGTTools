4af119a59bab0e955a3ac463bb4e381d0440469f
eliasfernandez <Socrats@users.noreply.github.com>
2021-11-12 18:41:39 +0100

    added new Detective strategy

--- egttools/behaviors/NormalForm/TwoActions/__init__.py
+++ egttools/behaviors/NormalForm/TwoActions/__init__.py
@@ -3,4 +3,4 @@ API reference documentation for `behaviors.NormalForm.TwoActions` submodule.
 """
 
 from egttools.numerical.behaviors.NormalForm.TwoActions import *
-from .nfg_strategies import EpsilonTFT, EpsilonGRIM
+from .nfg_strategies import EpsilonTFT, EpsilonGRIM, Detective
