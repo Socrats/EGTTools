fixed plot of trajectories and gradient for discrete dyanmics

egttools/plotting/helpers.py
@@ -6,6 +6,7 @@ from typing import Tuple, List, Callable, Optional, Union
 import numpy
 import numpy as np
 from scipy.optimize import root
+from egttools.numerical import calculate_nb_states, sample_simplex
 
 
 def simplex_iterator(scale, boundary=True):
@@ -177,8 +178,10 @@ def calculate_stability(roots: List[np.ndarray], f: Callable[[np.ndarray], np.nd
 def calculate_stationary_points(x: np.ndarray, y: np.ndarray, corners: np.ndarray,
                                 f: Callable[[np.ndarray], np.ndarray],
                                 border: Optional[int] = 5,
-                                delta: Optional[float] = 1e-12) -> Tuple[List[np.ndarray], List[np.ndarray]]:
+                                delta: Optional[float] = 1e-12,
+                                atol: Optional[float] = 1e-7) -> Tuple[List[np.ndarray], List[np.ndarray]]:
     """
+    Finds the roots of f, given a number of points.
 
     Parameters
     ----------
@@ -188,6 +191,7 @@ def calculate_stationary_points(x: np.ndarray, y: np.ndarray, corners: np.ndarra
     f
     border
     delta
+    atol
 
     Returns
     -------
@@ -198,17 +202,73 @@ def calculate_stationary_points(x: np.ndarray, y: np.ndarray, corners: np.ndarra
         start = xy_to_barycentric_coordinates(x, y, corners)
         sol = root(f, start, method="hybr")  # ,xtol=1.49012e-10,maxfev=1000
         if sol.success:
-            # check if FP is in simplex
-            if not np.isclose(np.sum(sol.x), 1., atol=1.e-3):
-                continue
-            if not np.all((sol.x > -delta) & (sol.x < 1 + delta)):  # only if fp in simplex
-                continue
-
-            # only add new fixed points to list
-            if not np.array([np.allclose(sol.x, x, atol=1e-7) for x in roots]).any():
-                roots.append(sol.x)
+            v = sol.x
+            if check_if_point_in_unit_simplex(v, delta):
+                # only add new fixed points to list
+                if not np.array([np.allclose(v, x, atol=atol) for x in roots]).any():
+                    roots.append(v)
 
     return roots, [barycentric_to_xy_coordinates(x, corners) for x in roots]
+
+
+def find_roots_in_discrete_barycentric_coordinates(f: Callable[[np.ndarray], np.ndarray],
+                                                   simplex_size: int,
+                                                   nb_edge_points: Optional[int] = None,
+                                                   nb_interior_points: Optional[int] = 1000,
+                                                   delta: Optional[float] = 1e-12,
+                                                   atol: Optional[float] = 1e-3) -> List[np.ndarray]:
+    roots = []
+
+    if nb_edge_points is None:
+        nb_edge_points = simplex_size
+
+    # We first test values along the edges
+    values = np.linspace(0, simplex_size, nb_edge_points, )
+    point = np.zeros(shape=(3,), dtype=np.int64)
+    for value in values:
+        for i in range(3):
+            point[i] = value
+            point[(i + 1) % 3] = simplex_size - value
+            point[(i + 2) % 3] = 0
+
+            sol = root(f, point, method="hybr")  # ,xtol=1.49012e-10,maxfev=1000
+            if sol.success:
+                v = sol.x / simplex_size
+                if check_if_point_in_unit_simplex(v, delta):
+                    # only add new fixed points to list
+                    if not np.array([np.allclose(v, x, atol=atol) for x in roots]).any():
+                        roots.append(v)
+
+    # finally we can explore values inside the simplex
+    nb_states = calculate_nb_states(simplex_size, 3)
+
+    if nb_interior_points > nb_states:
+        nb_interior_points = nb_states
+    initial_points = np.random.choice(range(nb_states), size=nb_interior_points, replace=False)
+
+    for i in initial_points:
+        point = sample_simplex(i, simplex_size, 3)
+        if np.any(np.isclose(point, 0., atol=1e-7)):
+            continue
+        sol = root(f, point, method="hybr")  # ,xtol=1.49012e-10,maxfev=1000
+        if sol.success:
+            v = sol.x / simplex_size
+            if check_if_point_in_unit_simplex(v, delta):
+                # only add new fixed points to list
+                if not np.array([np.allclose(v, x, atol=atol) for x in roots]).any():
+                    roots.append(v)
+
+    return roots
+
+
+def check_if_point_in_unit_simplex(point: np.ndarray, delta: Optional[float] = 1e-12) -> bool:
+    if not np.isclose(np.sum(point), 1., atol=1.e-2):
+        return False
+
+    if not np.all((point > -delta) & (point < 1 + delta)):  # only if fp in simplex
+        return False
+
+    return True
 
 
 def perturb_state(state: Union[Tuple[float, float, float], np.ndarray],
@@ -262,6 +322,75 @@ def perturb_state(state: Union[Tuple[float, float, float], np.ndarray],
             tmp = state.copy()
 
             if tmp[i] <= 1. - perturbation:
+                tmp[i] += perturbation
+                if tmp[(i + 1) % 3] >= perturbation:
+                    tmp[(i + 1) % 3] -= perturbation
+                else:
+                    tmp[(i + 2) % 3] -= perturbation
+                new_states.append(tmp)
+
+    return new_states
+
+
+def perturb_state_discrete(state: Union[Tuple[float, float, float], np.ndarray],
+                           size: int,
+                           perturbation: Optional[int] = 1) -> List[np.ndarray]:
+    """
+    Produces a number of points in the simplex close to the state.
+
+    If the sate is a vertex or in an edge, the perturbation is only made
+    across the edges (we don't look for points in the interior of the simplex).
+
+    Parameters
+    ----------
+    state
+    size
+    perturbation
+
+    Returns
+    -------
+    List[numpy.array]
+        A list of points (in barycentric coordinates) which are close to the state in the simplex.
+    """
+    new_states = []
+    point_location, indexes = find_where_point_is_in_simplex(state)
+    # first we check where the point is
+
+    if point_location == 0:
+        # If the point is a vertex, then we only perturb across that axis
+        tmp1 = state.copy()
+        tmp1[indexes[0]] -= perturbation
+        tmp1[(indexes[0] + 1) % 3] += perturbation
+        new_states.append(tmp1)
+        tmp2 = state.copy()
+        tmp2[indexes[0]] -= perturbation
+        tmp2[(indexes[0] + 2) % 3] += perturbation
+        new_states.append(tmp2)
+    elif point_location == 1:
+        # If the point in an edge, we will produce 2 points
+        if state[indexes[0]] >= perturbation:
+            tmp1 = state.copy()
+            if tmp1.sum() < size:
+                tmp1[indexes[0]] += size - tmp1.sum()
+            tmp1[indexes[0]] -= perturbation
+            tmp1[indexes[1]] += perturbation
+            new_states.append(tmp1)
+        if state[indexes[1]] >= perturbation:
+            tmp2 = state.copy()
+            if tmp2.sum() < size:
+                tmp2[indexes[0]] += size - tmp2.sum()
+            tmp2[indexes[0]] += perturbation
+            tmp2[indexes[1]] -= perturbation
+            new_states.append(tmp2)
+    else:
+        # if the point is in the interior of the simplex, we will
+        # produce 3 points, each in the direction of a vertex
+        for i in range(len(state)):
+            tmp = state.copy()
+            if tmp.sum() < size:
+                tmp[0] += size - tmp.sum()
+
+            if tmp[i] + perturbation <= size:
                 tmp[i] += perturbation
                 if tmp[(i + 1) % 3] >= perturbation:
                     tmp[(i + 1) % 3] -= perturbation

egttools/plotting/simplex2d.py
@@ -25,8 +25,9 @@ from scipy.integrate import odeint
 from matplotlib.patches import Circle
 from typing import Optional, Tuple, List, Union, Callable, TypeVar
 from numpy.typing import ArrayLike
-from egttools.numerical import sample_unit_simplex
-from egttools.plotting.helpers import barycentric_to_xy_coordinates, perturb_state, add_arrow
+from egttools.numerical import (sample_unit_simplex, sample_simplex, calculate_nb_states, )
+from egttools.plotting.helpers import (barycentric_to_xy_coordinates, perturb_state, add_arrow,
+                                       perturb_state_discrete, )
 
 top_corner = np.sqrt(3) / 2
 side_slope = np.sqrt(3)
@@ -44,7 +45,36 @@ class Simplex2D:
     refiner = tri.UniformTriRefiner(triangle)
     trimesh = refiner.refine_triangulation(subdiv=5)
 
-    def __init__(self, nb_points: Optional[int] = 1000):
+    def __init__(self, nb_points: Optional[int] = 1000, discrete: Optional[bool] = False,
+                 size: Optional[Union[int, None]] = None):
+        """
+        This class offers utility methods to plot gradients and equilibrium points on a 2-simplex (triangle).
+
+        The plotting is always done on the unit simplex for convenience. At the moment no rotations are
+        implemented, but we plan to add this feature, so that the triangle can be rotated before the plot.
+
+        We discern between continuous and discrete dynamics. The main reason is that this class' objective
+        is to plot evolutionary dynamics on a simplex. When we are working with the replicator equation
+        it is straightforward to calculate all the gradients on the unit simplex. However, when working
+        with finite populations using the social learning model (social imitation), we are actually working
+        with a simplex with size equivalent to the population size (so all the dimensions of the simplex must
+        sum to `Z`) and we only consider discrete (integer) values inside the simplex (the population may
+        only have integer individuals). Of course this can be translated into frequencies, which gets us
+        back to the unit simplex, but it is not so simple to transform any value between 0-1 sampled with
+        numpy.linspace to a discrete value.
+
+        Therefore, for the discrete case, will will sample directly discrete points in barycentric
+        coordinates and only then, translate them into cartesian cooordinates.
+
+        Parameters
+        ----------
+        nb_points : int
+            number of points for which to calculate the gradients
+        discrete : bool
+            indicates whether we are in the continuous or discrete case
+        size : int
+            if we are in the discrete case, indicates the size of the simplex
+        """
         self.nb_points = nb_points
         self.Ux = None
         self.Uy = None
@@ -54,6 +84,8 @@ class Simplex2D:
         self.X, self.Y = np.meshgrid(x, y)
         self.figure = None
         self.ax = None
+        self.discrete = discrete
+        self.size = size
 
     def add_axis(self, figsize: Optional[Tuple[int, int]] = (10, 8), ax: Optional[plt.axis] = None) -> SelfSimplex2D:
         if ax is not None:
@@ -162,12 +194,24 @@ class Simplex2D:
                           trajectory_length: Optional[int] = 15, step: Optional[float] = 0.01,
                           color: Optional[Union[str, Tuple[int, int, int]]] = 'whitesmoke',
                           ms: Optional[float] = 0.5, zorder: Optional[int] = 0) -> SelfSimplex2D:
-        for _ in range(nb_trajectories):
-            u = sample_unit_simplex(3)
-            x = odeint(f, u, np.arange(0, trajectory_length, step), full_output=False)
-            # noinspection PyTypeChecker
-            v = barycentric_to_xy_coordinates(x, self.corners)
-            self.ax.plot(v[:, 0], v[:, 1], color, ms=ms, zorder=zorder)
+        if self.discrete:
+            nb_states = calculate_nb_states(self.size, 3)
+            if nb_trajectories > nb_states:
+                nb_trajectories = nb_states
+            initial_points = np.random.choice(range(nb_states), size=nb_trajectories, replace=False)
+            for point in initial_points:
+                u = sample_simplex(point, self.size, 3)
+                x = odeint(f, u, np.arange(0, trajectory_length, step), full_output=False)
+                # noinspection PyTypeChecker
+                v = barycentric_to_xy_coordinates(x / self.size, self.corners)
+                self.ax.plot(v[:, 0], v[:, 1], color, ms=ms, zorder=zorder)
+        else:
+            for _ in range(nb_trajectories):
+                u = sample_unit_simplex(3)
+                x = odeint(f, u, np.arange(0, trajectory_length, step), full_output=False)
+                # noinspection PyTypeChecker
+                v = barycentric_to_xy_coordinates(x, self.corners)
+                self.ax.plot(v[:, 0], v[:, 1], color, ms=ms, zorder=zorder)
 
         return self
 
@@ -194,24 +238,45 @@ class Simplex2D:
     def draw_trajectory_from_roots(self, f: Callable[[np.ndarray, int], np.ndarray], roots: List[np.ndarray],
                                    stability: List[np.ndarray],
                                    trajectory_length: Optional[int] = 15, step: Optional[float] = 0.1,
+                                   perturbation: Optional[Union[int, float]] = 0.01,
                                    color: Optional[Union[str, Tuple[int, int, int]]] = 'k',
                                    linewidth: Optional[float] = 0.5, zorder: Optional[int] = 0,
                                    draw_arrow: Optional[bool] = False, arrowstyle: Optional[str] = 'fancy',
                                    arrowsize: Optional[int] = 50,
                                    position: Optional[int] = None,
                                    arrowdirection: Optional[str] = 'right') -> SelfSimplex2D:
-        for i, stationary_point in enumerate(roots):
-            if stability[i]:  # we don't plot arrows starting at stable points
-                continue
-            states = perturb_state(stationary_point)
-            for state in states:
-                x = odeint(f, state, np.arange(0, trajectory_length, step), full_output=False)
+        if self.discrete:
+            if type(perturbation) is float:
+                perturbation = 1
 
-                # noinspection PyTypeChecker
-                v = barycentric_to_xy_coordinates(x, self.corners)
-                line = self.ax.plot(v[:, 0], v[:, 1], color, linewidth=linewidth, zorder=zorder)[0]
-                if draw_arrow:
-                    add_arrow(line, size=arrowsize, arrowstyle=arrowstyle, position=position, direction=arrowdirection)
+            for i, stationary_point in enumerate(roots):
+                if stability[i]:  # we don't plot arrows starting at stable points
+                    continue
+                stationary_point_discrete = (stationary_point * self.size)
+                states = perturb_state_discrete(stationary_point_discrete, self.size, perturbation=perturbation)
+                for state in states:
+                    x = odeint(f, state, np.arange(0, trajectory_length, step), full_output=False)
+
+                    # noinspection PyTypeChecker
+                    v = barycentric_to_xy_coordinates(x / self.size, self.corners)
+                    line = self.ax.plot(v[:, 0], v[:, 1], color, linewidth=linewidth, zorder=zorder)[0]
+                    if draw_arrow:
+                        add_arrow(line, size=arrowsize, arrowstyle=arrowstyle, position=position,
+                                  direction=arrowdirection)
+        else:
+            for i, stationary_point in enumerate(roots):
+                if stability[i]:  # we don't plot arrows starting at stable points
+                    continue
+                states = perturb_state(stationary_point, perturbation=perturbation)
+                for state in states:
+                    x = odeint(f, state, np.arange(0, trajectory_length, step), full_output=False)
+
+                    # noinspection PyTypeChecker
+                    v = barycentric_to_xy_coordinates(x, self.corners)
+                    line = self.ax.plot(v[:, 0], v[:, 1], color, linewidth=linewidth, zorder=zorder)[0]
+                    if draw_arrow:
+                        add_arrow(line, size=arrowsize, arrowstyle=arrowstyle, position=position,
+                                  direction=arrowdirection)
 
         return self
 
@@ -220,11 +285,24 @@ class Simplex2D:
                             s: Optional[Union[float, ArrayLike]] = 0.1,
                             color: Optional[Union[str, Tuple[int, int, int]]] = 'whitesmoke',
                             marker: Optional[str] = '.', zorder: Optional[int] = 0) -> SelfSimplex2D:
-        for _ in range(nb_trajectories):
-            u = sample_unit_simplex(3)
-            x = odeint(f, u, np.arange(0, trajectory_length, step), full_output=False)
-            # noinspection PyTypeChecker
-            v = barycentric_to_xy_coordinates(x, self.corners)
-            self.ax.scatter(v[:, 0], v[:, 1], s, color=color, marker=marker, zorder=zorder)
+
+        if self.discrete:
+            nb_states = calculate_nb_states(self.size, 3)
+            if nb_trajectories > nb_states:
+                nb_trajectories = nb_states
+            initial_points = np.random.choice(range(nb_states), size=nb_trajectories, replace=False)
+            for point in initial_points:
+                u = sample_simplex(point, self.size, 3)
+                x = odeint(f, u, np.arange(0, trajectory_length, step), full_output=False)
+                # noinspection PyTypeChecker
+                v = barycentric_to_xy_coordinates(x / self.size, self.corners)
+                self.ax.scatter(v[:, 0], v[:, 1], s, color=color, marker=marker, zorder=zorder)
+        else:
+            for _ in range(nb_trajectories):
+                u = sample_unit_simplex(3)
+                x = odeint(f, u, np.arange(0, trajectory_length, step), full_output=False)
+                # noinspection PyTypeChecker
+                v = barycentric_to_xy_coordinates(x, self.corners)
+                self.ax.scatter(v[:, 0], v[:, 1], s, color=color, marker=marker, zorder=zorder)
 
         return self