db5624f5ff1f667fff37c556f646fc26b11e4aea
eliasfernandez <Socrats@users.noreply.github.com>
2022-10-25 07:52:27 +0200

    small changes to the ci pipeline. No more need to move tests to another location.

--- build_tools/github/check_if_egttools_is_installed.py
+++ build_tools/github/check_if_egttools_is_installed.py
@@ -1,8 +1,8 @@
 try:
-    from src import egttools
+    import egttools
 except ImportError as e:
     print('egttools is not installed')
 else:
     print('found egttools')
     print('Numerical module in: {}'.format(egttools.numerical))
-    print(egttools.numerical.behaviors.NormalForm.TwoActions.Random())
+    print(egttools.numerical.numerical.behaviors.NormalForm.TwoActions.Random())
