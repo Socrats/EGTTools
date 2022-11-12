4a919376b04d73acd3e766a38300be4c61ccdc74
eliasfernandez <Socrats@users.noreply.github.com>
2022-11-12 13:22:15 +0100

    added tests for PairwiseComparison

--- tests/test_analytical.py
+++ tests/test_analytical.py
@@ -8,8 +8,6 @@ def test_analytical_fitness_calculation():
     c = 0.1
     b = 1
     Z = 50
-    beta = 2
-    pop_states = np.arange(0, Z + 1, 1)
     risk = 1.
     game = egt.games.OneShotCRD(b, c, risk, N, M)
     payoffs = egt.utils.transform_payoffs_to_pairwise(game.nb_strategies(), game)
@@ -17,3 +15,49 @@ def test_analytical_fitness_calculation():
     result = evolver.fitness_group(Z - 1, 0, 1)
 
     assert result > 0
+
+
+def test_analytical_pairwise_comparison_generic_game():
+    N = 6
+    M = 3
+    c = 0.1
+    b = 1
+    Z = 50
+    risk = 1.
+    game = egt.games.OneShotCRD(b, c, risk, N, M)
+    evolver = egt.analytical.PairwiseComparison(Z, game)
+    result = evolver.calculate_fixation_probability(0, 1, 1)
+
+    assert np.isclose(0.003682100470889826, result)
+
+
+def test_analytical_pairwise_comparison_2_player_game():
+    # Payoff matrix
+    V = 2
+    D = 3
+    T = 1
+    A = np.array([
+        [(V - D) / 2, V],
+        [0, (V / 2) - T],
+    ])
+    game = egt.games.Matrix2PlayerGameHolder(2, payoff_matrix=A)
+    evolver = egt.analytical.PairwiseComparison(100, game)
+    result = evolver.calculate_fixation_probability(0, 1, 1)
+
+    assert np.isclose(0.8641155742462664, result)
+
+
+def test_analytical_pairwise_comparison_n_player_game():
+    # Payoff matrix
+    V = 2
+    D = 3
+    T = 1
+    A = np.array([
+        [(V - D) / 2, V, V/2, D],
+        [0, (V / 2) - T, T, V],
+    ])
+    game = egt.games.MatrixNPlayerGameHolder(nb_strategies=2, group_size=3, payoff_matrix=A)
+    evolver = egt.analytical.PairwiseComparison(100, game)
+    result = evolver.calculate_fixation_probability(1, 0, 1)
+
+    assert np.isclose(0.020104159364630978, result)
