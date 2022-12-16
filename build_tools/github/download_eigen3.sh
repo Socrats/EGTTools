ec0996e9acb60f97af7dac4dbac32d6fed2c93ac
eliasfernandez <Socrats@users.noreply.github.com>
2022-12-16 12:44:03 +0100

    going back to yum install

--- build_tools/github/download_eigen3.sh
+++ build_tools/github/download_eigen3.sh
@@ -20,10 +20,10 @@ cmake ..
 make install
 
 # Download and install Boost 1.80.0
-curl -O https://boostorg.jfrog.io/artifactory/main/release/1.80.0/source/boost_1_80_0.tar.gz
-tar xf boost_1_80_0.tar.gz
-mv boost_1_80_0 boost
-cd boost
-./bootstrap.sh
-./bjam install
-#yum install boost-devel
+#curl -O https://boostorg.jfrog.io/artifactory/main/release/1.80.0/source/boost_1_80_0.tar.gz
+#tar xf boost_1_80_0.tar.gz
+#mv boost_1_80_0 boost
+#cd boost
+#./bootstrap.sh
+#./bjam install
+yum install boost-devel
