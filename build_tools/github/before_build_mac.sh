63202a3129600e3147946670c6209fbc929c4378
eliasfernandez <Socrats@users.noreply.github.com>
2023-03-14 18:31:40 +0100

    attempting to set MACOSX_DEPLOYMENT_TARGET through $GITHUB_ENV

--- build_tools/github/before_build_mac.sh
+++ build_tools/github/before_build_mac.sh
@@ -14,9 +14,9 @@ brew install boost
 if [[ $(uname) == "Darwin" ]]; then
   if [[ "$CIBW_BUILD" == *-macosx_arm64 ||  "$CIBW_BUILD" == *-macosx_universal2 ]]; then
     export MACOSX_DEPLOYMENT_TARGET=12.0
-    export EGTTOOLS_EXTRA_CMAKE_ARGS='-DCMAKE_OSX_DEPLOYMENT_TARGET:STRING=12.0'
+    echo "MACOSX_DEPLOYMENT_TARGET=12.0" >> "$GITHUB_ENV"
   else
     export MACOSX_DEPLOYMENT_TARGET=10.9
-    export EGTTOOLS_EXTRA_CMAKE_ARGS='-DCMAKE_OSX_DEPLOYMENT_TARGET:STRING=10.9'
+    echo "MACOSX_DEPLOYMENT_TARGET=10.9" >> "$GITHUB_ENV"
   fi
 fi
