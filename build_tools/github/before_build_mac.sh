212402ea213549cea8cdfedbddc6711df86cd37d
eliasfernandez <Socrats@users.noreply.github.com>
2023-09-29 18:02:53 +0200

    trying to add EGTTOOLS_EXTRA_CMAKE_ARGS to the yml file

--- build_tools/github/before_build_mac.sh
+++ build_tools/github/before_build_mac.sh
@@ -15,13 +15,13 @@ if [[ $(uname) == "Darwin" ]]; then
   if [[ "$CIBW_BUILD" == *-macosx_arm64 || "$CIBW_BUILD" == *-macosx_universal2:arm64 ]]; then
     brew reinstall libomp
     export MACOSX_DEPLOYMENT_TARGET=12.0
-    export EGTTOOLS_EXTRA_CMAKE_ARGS="-DLIBOMP_DIR='$(brew --prefix libomp)' -DMACOSX_DEPLOYMENT_TARGET=12.0"
+    export EGTTOOLS_EXTRA_CMAKE_ARGS="${EGTTOOLS_EXTRA_CMAKE_ARGS} -DMACOSX_DEPLOYMENT_TARGET=12.0"
     echo "MACOSX_DEPLOYMENT_TARGET=12.0" >>"$GITHUB_ENV"
 
   else
     brew reinstall libomp
     export MACOSX_DEPLOYMENT_TARGET=10.15
-    export EGTTOOLS_EXTRA_CMAKE_ARGS="-DLIBOMP_DIR='$(brew --prefix libomp)' -DMACOSX_DEPLOYMENT_TARGET=10.15"
+    export EGTTOOLS_EXTRA_CMAKE_ARGS="${EGTTOOLS_EXTRA_CMAKE_ARGS} -DMACOSX_DEPLOYMENT_TARGET=10.15"
     echo "MACOSX_DEPLOYMENT_TARGET=10.15" >>"$GITHUB_ENV"
   fi
 fi
