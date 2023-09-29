9cd51a60a11f5dd90e400691aaec2c0870f7dc1a
eliasfernandez <Socrats@users.noreply.github.com>
2023-09-29 17:29:15 +0200

    syntax issue

--- build_tools/github/before_build_mac.sh
+++ build_tools/github/before_build_mac.sh
@@ -13,18 +13,16 @@ brew install boost
 
 if [[ $(uname) == "Darwin" ]]; then
   if [[ "$CIBW_BUILD" == *-macosx_arm64 || "$CIBW_BUILD" == *-macosx_universal2:arm64 ]]; then
+    brew reinstall libomp
     export MACOSX_DEPLOYMENT_TARGET=12.0
-    export EGTTOOLS_EGTTOOLS_EXTRA_CMAKE_ARGS='-DMACOSX_DEPLOYMENT_TARGET=12.0 -DLIBOMP_DIR='$(brew --prefix libomp)''
+    export EGTTOOLS_EGTTOOLS_EXTRA_CMAKE_ARGS="-DMACOSX_DEPLOYMENT_TARGET=12.0 -DLIBOMP_DIR='$(brew --prefix libomp)'"
     echo "MACOSX_DEPLOYMENT_TARGET=12.0" >>"$GITHUB_ENV"
 
-    brew reinstall libomp
-
   else
-    export MACOSX_DEPLOYMENT_TARGET=10.15
-    export EGTTOOLS_EGTTOOLS_EXTRA_CMAKE_ARGS='-DMACOSX_DEPLOYMENT_TARGET=10.15 -DLIBOMP_DIR='$(brew --prefix libomp)''
-    echo "MACOSX_DEPLOYMENT_TARGET=10.15" >>"$GITHUB_ENV"
-
     brew reinstall libomp
+    export MACOSX_DEPLOYMENT_TARGET=10.15
+    export EGTTOOLS_EGTTOOLS_EXTRA_CMAKE_ARGS="-DMACOSX_DEPLOYMENT_TARGET=10.15 -DLIBOMP_DIR='$(brew --prefix libomp)'"
+    echo "MACOSX_DEPLOYMENT_TARGET=10.15" >>"$GITHUB_ENV"
   fi
 fi
 
