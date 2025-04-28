fd9b9734b4486b45252f52a57b9866153554f9cf
eliasfernandez <Socrats@users.noreply.github.com>
2025-04-28 21:51:06 +0200

    setting MACOSX_DEPLOYMENT_TARGET inside CIBW_ENVIRONMENT_MACOS

--- build_tools/github/set_macosx_deployment_target.sh
+++ build_tools/github/set_macosx_deployment_target.sh
@@ -8,8 +8,10 @@ echo "Detected architecture: ${arch_name}"
 
 if [[ "$arch_name" == "arm64" ]]; then
   export MACOSX_DEPLOYMENT_TARGET=14.0
+  echo "MACOSX_DEPLOYMENT_TARGET=14.0" >> $GITHUB_ENV
   echo "Set MACOSX_DEPLOYMENT_TARGET=14.0 for ARM64"
 else
   export MACOSX_DEPLOYMENT_TARGET=11.0
+  echo "MACOSX_DEPLOYMENT_TARGET=11.0" >> $GITHUB_ENV
   echo "Set MACOSX_DEPLOYMENT_TARGET=11.0 for Intel x86_64"
 fi
